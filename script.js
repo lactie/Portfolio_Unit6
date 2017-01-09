6a7061a92c41de94272227cd2dbe328c816e051a
Louisa Actie <lactie@fpanyc.org>
2017-01-09 15:18:21 +0000

    i changed stuff

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-$(document).ready(function(){
+$("document").ready(function(){}
     //Write all your jQuery code here
         $("button").click(function() {
+        }
       $("#like").html("You like this project!");
     });
-    
-    
-    
-    
-});
